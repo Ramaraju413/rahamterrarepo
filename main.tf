1110d4a1fd83ac8924aa36463cdabb67334989db
Ramaraju413 <ramaraju086@gmail.com>
2025-10-30 23:34:24 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ region = "us-east-1"
 
 resource "aws_instance" "three" {
   count         = 2
-  ami           = "ami-0341d95f75f311023"
+  ami           = "ami-0bdd88bd06d16ba03"
   instance_type = "t3.micro"
 
   tags = {
